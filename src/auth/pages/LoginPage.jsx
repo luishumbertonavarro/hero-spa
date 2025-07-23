1954cbfa55c1b5a08760a47636f95162c24b1122
Luis Humberto Navarro <ribeiro.beton@gmail.com>
2025-07-23 17:11:32 -0300

    proyecto casi terminado

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,21 +1,33 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
 
-  const navigate=useNavigate();
 
-  const onLogin=()=>{
-    navigate('/',{
-      replace:true
+  const { login } = useContext(AuthContext)
+  const navigate = useNavigate();
+
+
+  const onLogin = () => {
+
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+
+    login('Luis Humberto');
+
+    navigate(lastPath, {
+      replace: true
     });
-
   }
+
+
   return (
     <div className="container mt-5">
       <h1>Login</h1>
       <hr />
       <button className='btn btn-primary'
-      onClick={onLogin}>
+        onClick={onLogin}>
         Login
       </button>
     </div>
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
+import { useContext } from 'react';
 
 
 export const Navbar = () => {
 
     const navigate = useNavigate();
+    const {user,logout} = useContext(AuthContext)
 
 
     const onLogout = () => {
+        logout();
         navigate('/login', {
             replace: true
         });
@@ -56,7 +60,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        betao
+                        {user?.name}
                     </span>
                     <button
                         className='nav-item nav-link btn'
